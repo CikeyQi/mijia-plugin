34f2f073f93c0494c488b5f0e0b2ec78dd16ab80
ZeroCal Su <3146312184@qq.com>
2024-05-10 02:54:09 +0800

    细节修复

--- guoba.support.js
+++ guoba.support.js
@@ -6,19 +6,20 @@ import { pluginRoot } from "./model/path.js";
 export function supportGuoba() {
   return {
     pluginInfo: {
-      name: 'mijia-plugin',
+      name: '米家控制插件',
       title: 'mijia-plugin',
       author: ['@CikeyQi'],
       authorLink: ['https://github.com/CikeyQi'],
       link: 'https://github.com/CikeyQi/mijia-plugin',
       isV3: true,
       isV2: false,
-      description: '基于Yunzai-Bot的米家插件，在任何地方控制家中米家生态设备',
+      showInMenu: true,
+      description: '基于 Yunzai 的小米智能家居控制插件',
       // 显示图标，此为个性化配置
       // 图标可在 https://icon-sets.iconify.design 这里进行搜索
-      icon: 'mdi:stove',
+      icon: 'cbi:xiaomi-logo',
       // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
-      iconColor: '#d19f56',
+      iconColor: '#19cd90',
       // 如果想要显示成图片，也可以填写图标路径（绝对路径）
       iconPath: path.join(pluginRoot, 'resources/readme/girl.png'),
     },
